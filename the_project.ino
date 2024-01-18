2a4f53a6c1c8d6f7bea3ef416144cf850f86b8f0
Peter Rozmaryn <119956876+peter3048@users.noreply.github.com>
2024-01-18 11:13:13 -0500

    Update the_project.ino
    yes

--- the_project.ino
+++ the_project.ino
@@ -7,47 +7,84 @@ int led4 = 5;
 int led5 = 6;
 int flashlight = 7;
 int crank = A1;
+int button = A2;
 
 
 int power = 0;
 
 
 void setup() {
-  pinMode(led, OUTPUT);
+  
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(flashlight, OUTPUT);
   pinMode(crank, INPUT);
+  pinMode(button, INPUT_PULLUP);
+
 }
 
 void loop() {
   if (digitalRead(crank) == HIGH){
     power++;
+    delay(50);
   }
-  // add some delay
+  
+  //this stuff happens when the button is pressed
+  if((digitalRead(button) == LOW) && (power > 0)){
+    digitalWrite(flashlight, LOW);
+    power--;
+    delay(50);
+    
+  }
+
+
+  else{
+    digitalWrite(flashlight,HIGH);
+  }
+  setLights();
+
+
+
+
 
   // make button that turns on light,  and drains power and leds go down
   setLights();
-  
-
 }
 void setLights(){
-  if(power >= 20){
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+
+
+
+  if(power >= 1){
     digitalWrite(led1, HIGH);
   }
-  if(power >= 40){
+  if(power >= 20){
     digitalWrite(led2, HIGH);
   }
-  if(power >= 60){
+  if(power >= 40){
     digitalWrite(led3, HIGH);
   }
-  if(power >= 80){
+  if(power >= 60){
     digitalWrite(led4, HIGH);
   }
-  if(power >= 100){
+  if(power >= 80){
     digitalWrite(led5, HIGH);
   }
 
 
   if(power>100){
     power = 100;
+  }
+  if(power<0){
+    power=0;
   }
 
 }
