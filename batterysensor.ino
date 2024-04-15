cd67a3bd6c22937ff564f71f585287686fe7e828
Peter Rozmaryn <119956876+peter3048@users.noreply.github.com>
2024-04-14 20:01:44 -0400

    Update batterysensor.ino

--- batterysensor.ino
+++ batterysensor.ino
@@ -1,27 +1,124 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+int led4 = 5;
+int led5 = 6;
+int usb = 7;
+int usbc1 = 8;
+int usbc2 = 9;
+int sensing = A1;
+int powertest = A2;
+int button = 10;
 
-
-const int rs = 12, en = 13, d0 = A0, d1 = A1, d2 = A2, d3 = A3;
-const int analogPin = A4;
+float val;
 float analogValue;
 float input_voltage;
 float max_voltage = 5;
 float step = max_voltage/5;
 
-int ledPins[] = {2, 3, 4, 5, 6}; // an array of pin numbers to which LEDs are attached
-int pinCount = 10;           // the number of pins (i.e. the length of the array)
 
-void setup()
-{
-   Serial.begin(9600);     //  opens serial port, sets data rate to 9600 bps
-   pinMode(A0,OUTPUT);
-   pinMode(A1,OUTPUT);
-   pinMode(A2,OUTPUT);
-   pinMode(A3,OUTPUT);
-   pinMode(A4,INPUT);
+int power = 100;
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(usb, OUTPUT);
+  pinMode(usbc1, OUTPUT);
+  pinMode(usbc2, OUTPUT);
+  pinMode(button, INPUT);
+  pinMode(sensing, INPUT);
+  pinMode(powertest, INPUT);
+
+
+  digitalWrite(usb, HIGH);
+  digitalWrite(usbc1, HIGH);
+  digitalWrite(usbc2, HIGH);
 
 }
 
-void LED_function(int stage)
+void loop() {
+  
+  if(digitalRead(button) == HIGH){ //make sure this is set right, might need to make it a pull up 
+    powerSense();
+    delay(5000); //keep lights on for 5 seconds
+  }
+  else if(powerIn()){
+    chargeSequence();
+  }
+  else{
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+  }        
+}
+
+boolean powerIn(){ // determines if power is coming in 
+  //see if power is greater than zero, probably need a tie down to keep it zero if no power is coming in. 
+  val = analogRead (powertest);
+  val = (val * 5.0) / 1024.0;
+  return val > 0;
+}
+
+void chargeSequence(){ //blinks the lights to show that the battery is being charged
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  delay(500);
+  if(!powerIn())
+    break;
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  delay(500);
+  if(!powerIn())
+    break;
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  delay(500);
+  if(!powerIn())
+    break;
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  delay(500);
+  if(!powerIn())
+    break;
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, LOW);
+  delay(500);
+  if(!powerIn())
+    break;
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH;
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  delay(500);
+  if(!powerIn())
+    break;
+}
+
+void LED_function(int stage) //this sets the leds based on the ouput of power sense
 {
     for (int j=2; j<=6; j++)
     {
@@ -29,21 +126,16 @@ void LED_function(int stage)
     }
     for (int i=1, l=2; i<=stage; i++,l++)
     {
-    digitalWrite(l,HIGH);  //delay(30);
-    Serial.print("x");
+    digitalWrite(l,HIGH);
     }
-    Serial.println();
    
 }
-void loop()
-{
-//  Conversion formula for voltage
-  analogValue = analogRead (A4);
-  delay (1000); 
-  input_voltage = (analogValue * 5.0) / 1024.0;
+
+void powerSense(){ //reads the power
+  analogValue = analogRead (sensing);
+  input_voltage = (analogValue * 5.0) / 1024.0; //this stuff is all for conversion
   
   Serial.println(input_voltage);
-  delay(100);
 
   if (input_voltage < step && input_voltage > 0.00 )
     {
